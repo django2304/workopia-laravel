a758054247440baf954b0a9ec8955cf750823455
django2304 <chichilinskiy@gmail.com>
2025-02-16 00:49:51 +0200

    Create eloquent relationships between tables, modifying Job, User models and apply migration

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
@@ -27,6 +28,13 @@ class Job extends Model
         'company_name',
         'company_description',
         'company_logo',
-        'company_website'
+        'company_website',
+        'user_id'
     ];
+
+    // Relation to user
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
